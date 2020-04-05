dda5333dc4d3df79463070af3649a5a895c7d93f
Abhinav <abhinavarora@live.com>
2020-04-05 19:36:56 +0530

    savepoint

--- android/app/src/main/java/com/firstdraft/MainApplication.java
+++ android/app/src/main/java/com/firstdraft/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.firstdraft;
 
 import android.app.Application;
 import android.net.Uri;
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.awesomeproject.generated.BasePackageList;
+import com.firstdraft.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
